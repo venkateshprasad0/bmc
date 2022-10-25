5582747cf476116b3d331356de4bb435e287a650
Venkatesh Prasad V <venkateshprasad0@gmail.com>
2022-10-25 12:48:40 +0530

    initial commit

---
+++ src/main/java/com/upgrad/bookmyconsultation/repository/UserRepository.java
@@ -0,0 +1,20 @@
+package com.upgrad.bookmyconsultation.repository;
+
+import com.upgrad.bookmyconsultation.entity.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, String> {
+
+	@Override
+	List<User> findAll();
+
+    User findByEmailId(String email);
+
+    //specify a method that returns User by finding it by email id
+	
+
+}

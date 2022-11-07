a3a5d8f07d628cf428591f2bf2cc61894abe4e35
Venkatesh Prasad V <venkateshprasad0@gmail.com>
2022-11-07 07:52:43 +0530

    Added exception handler

--- src/main/java/com/upgrad/bookmyconsultation/controller/AppointmentController.java
+++ src/main/java/com/upgrad/bookmyconsultation/controller/AppointmentController.java
@@ -6,13 +6,9 @@ import com.upgrad.bookmyconsultation.exception.SlotUnavailableException;
 import com.upgrad.bookmyconsultation.service.AppointmentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin("*")
 @RestController
 @RequestMapping("/appointments")
 public class AppointmentController {
--- src/main/java/com/upgrad/bookmyconsultation/controller/RatingsController.java
+++ src/main/java/com/upgrad/bookmyconsultation/controller/RatingsController.java
@@ -4,12 +4,9 @@ import com.upgrad.bookmyconsultation.entity.Rating;
 import com.upgrad.bookmyconsultation.service.RatingsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-
+@CrossOrigin("*")
 @RestController
 @RequestMapping("/ratings")
 public class RatingsController {
